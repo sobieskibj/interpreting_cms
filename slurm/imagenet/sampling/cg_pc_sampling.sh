811481b8948fdeb80293e552a4f9dbfc69f1c78c
sobieskibj <sobieski.bartlomiej.jan@gmail.com>
2024-06-03 10:39:18 +0200

    Add lsun cat sampling

--- slurm/imagenet/sampling/cg_pc_sampling.sh
+++ slurm/imagenet/sampling/cg_pc_sampling.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
 #SBATCH --mem=40G
-#SBATCH --time 12:00:00
+#SBATCH --time 24:00:00
 #SBATCH --job-name=icm
 #SBATCH --output=slurm_logs/icm-%A.log
 
@@ -30,10 +30,6 @@ for T in $(seq 5 5 200); do
 
     srun python src/main.py \
     --config-name cm_sampling_cg_pc_imagenet \
-    "exp.ts=[0,$T]" \
-    'exp.ks=[31,31]' \
-    exp.batch_size=2 \
-    exp.n_samples=2 \
-    exp.step_size=0.01
+    "exp.ts=[0,$T]"
 
 done
--- src/sampler/cm/cg_per_step_pc.py
+++ src/sampler/cm/cg_per_step_pc.py
@@ -1,10 +1,11 @@
+import math
 import torch
 import numpy as np
 
 @torch.no_grad()
 def cg_per_step_pc(
         distiller, x, sigmas, ts, ks, clf, target_class_id, grad_at_p, grad_at_c, step_size,
-        t_min = 0.002, t_max = 80.0, rho = 7.0, steps = 40, fix_noise = False):
+        scheduler_type, t_min = 0.002, t_max = 80.0, rho = 7.0, steps = 40, fix_noise = False):
     '''
     ks - number of corrector steps per each predictor step
     '''
@@ -19,10 +20,16 @@ def cg_per_step_pc(
     def get_clf_grad(x):
         x.requires_grad_()
         logits = clf(normalize(x))
-        log_probs = F.log_softmax(logits, dim = 0)
+        log_probs = F.log_softmax(logits, dim = -1)
         target_class_log_probs = log_probs[:, target_class_id].unsqueeze(1)
         grad = torch.autograd.grad(target_class_log_probs.sum(), x)[0]
         return grad
+
+    def step_size_scheduler(init_step_size, total_step, scheduler_type):
+        if scheduler_type == 'cos':
+            return lambda step: 0.5 * init_step_size * (1 + math.cos(math.pi * (step / total_step)))
+        elif scheduler_type == 'const':
+            return lambda step: init_step_size
 
     t_max_rho = t_max ** (1 / rho)
     t_min_rho = t_min ** (1 / rho)
@@ -32,6 +39,16 @@ def cg_per_step_pc(
     if fix_noise:
         noise = x.clone()
 
+    if grad_at_p and grad_at_c:
+        total_step = sum([e + 1 for e in ks])
+    elif grad_at_p and not grad_at_c:
+        total_step = len(ts)
+    elif not grad_at_p and grad_at_c:
+        total_step = sum([e for e in ks])
+
+    step = 0
+    scheduler = step_size_scheduler(step_size, total_step, scheduler_type)
+
     for i, k in zip(range(len(ts)), ks):
 
         if not fix_noise:
@@ -39,6 +56,7 @@ def cg_per_step_pc(
 
         # get variance for ith iteration of predictor
         t = (t_max_rho + ts[i] / (steps - 1) * (t_min_rho - t_max_rho)) ** rho
+        t = np.clip(t, t_min, t_max)
 
         # add properly scaled noise for all iterations except the first one
         # where x comes from standard gaussian and we scale it with highest variance
@@ -53,7 +71,10 @@ def cg_per_step_pc(
 
         # optionally move along clf gradient
         if grad_at_p:
-            x0 += step_size * get_clf_grad(x0)
+            step_size_ = scheduler(step)
+            step += 1
+            grad = get_clf_grad(x0)
+            x0 += step_size_ * grad
 
         # iterate over corrector steps
         for c_iter in range(k):
@@ -70,6 +91,9 @@ def cg_per_step_pc(
 
             # optionally move along clf gradient
             if grad_at_c:
-                x0 += step_size * get_clf_grad(x0)
+                step_size_ = scheduler(step)
+                step += 1
+                grad = get_clf_grad(x0)
+                x0 += step_size_ * grad
 
     return torch.cat(xs, 0)
