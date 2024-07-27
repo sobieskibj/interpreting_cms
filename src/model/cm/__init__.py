0babff1b3f0c5741af74969b15c86736004d5653
OneAndZero24 <oneandzero24@gmail.com>
2024-07-27 20:35:02 +0200

    fix: import samplers

--- src/model/cm/__init__.py
+++ src/model/cm/__init__.py
@@ -1,5 +1,5 @@
 from .src.unet import UNetModel
 from .src.unet_h_move import UNetModelHMove
 from .src.karras_diffusion import KarrasDenoiser
-from .src.karras_samplers import *
+from sampler.cm import *
 from .src.resample import *
